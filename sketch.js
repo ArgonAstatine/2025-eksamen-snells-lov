a4985bf497c7c7941551e395ecc1035643422194
Bigladfike <dafikflopstar@gmail.com>
2025-03-26 16:49:36 +0100

    NY CUSTOM N1 OG N2 VÆRDIER SLIDERS YEAA BABYYYY

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 let angleSlider;
+let customN1Slider, customN2Slider;
 let n1 = 1.0;
 let n2 = 1.51;
 let canvasWidth = 1920;
 let canvasHeight = 1080;
 let materialSelect1, materialSelect2;
+let customN1 = false, customN2 = false;
 
 let materials = {
     "Luft": 1.00,
@@ -19,34 +21,51 @@ function setup() {
     createCanvas(canvasWidth, canvasHeight);
     angleMode(DEGREES);
     
-    // Opret en skyder til at justere indfaldsvinklen
     angleSlider = createSlider(0, 90, 30, 1);
     angleSlider.position(20, 20);
     angleSlider.style('width', '200px');
     
-    // Dropdown menu for at vælge materiale til n1
     materialSelect1 = createSelect();
     materialSelect1.position(20, 50);
     for (let mat in materials) {
         materialSelect1.option(mat);
     }
     materialSelect1.selected("Luft");
+    materialSelect1.changed(() => customN1 = false);
     
-    // Dropdown menu for at vælge materiale til n2
     materialSelect2 = createSelect();
     materialSelect2.position(20, 80);
     for (let mat in materials) {
         materialSelect2.option(mat);
     }
     materialSelect2.selected("Rudeglas");
+    materialSelect2.changed(() => customN2 = false);
+    
+    customN1Slider = createSlider(1.0, 10.0, 1.0, 0.01);
+    customN1Slider.position(20, 110);
+    customN1Slider.input(() => customN1 = true);
+    
+    customN2Slider = createSlider(1.0, 10.0, 1.51, 0.01);
+    customN2Slider.position(20, 140);
+    customN2Slider.input(() => customN2 = true);
 }
 
 function draw() {
     background(220);
     
     let incidentAngle = angleSlider.value();
-    n1 = materials[materialSelect1.value()];
-    n2 = materials[materialSelect2.value()];
+    if (customN1) {
+        n1 = customN1Slider.value();
+    } else {
+        n1 = materials[materialSelect1.value()];
+    }
+    
+    if (customN2) {
+        n2 = customN2Slider.value();
+    } else {
+        n2 = materials[materialSelect2.value()];
+    }
+    
     let refractedAngle = asin((n1 / n2) * sin(incidentAngle));
     
     drawInterface(incidentAngle, refractedAngle);
@@ -61,10 +80,10 @@ function drawInterface(incidentAngle, refractedAngle) {
     
     fill(0);
     textSize(24);
-    text(`Indfaldsvinkel: ${incidentAngle.toFixed(1)}°`, 20, 120);
-    text(`Brydningsvinkel: ${isNaN(refractedAngle) ? 'Totalrefleksion' : refractedAngle.toFixed(1) + '°'}`, 20, 150);
-    text(`Materiale 1: ${materialSelect1.value()} (n1 = ${n1.toFixed(2)})`, 20, 180);
-    text(`Materiale 2: ${materialSelect2.value()} (n2 = ${n2.toFixed(2)})`, 20, 210);
+    text(`Indfaldsvinkel: ${incidentAngle.toFixed(1)}°`, 20, 180);
+    text(`Brydningsvinkel: ${isNaN(refractedAngle) ? 'Totalrefleksion' : refractedAngle.toFixed(1) + '°'}`, 20, 210);
+    text(`Materiale 1: ${customN1 ? "Custom" : materialSelect1.value()} (n1 = ${n1.toFixed(2)})`, 20, 240);
+    text(`Materiale 2: ${customN2 ? "Custom" : materialSelect2.value()} (n2 = ${n2.toFixed(2)})`, 20, 270);
 }
 
 function drawLightRays(incidentAngle, refractedAngle) {
@@ -72,21 +91,18 @@ function drawLightRays(incidentAngle, refractedAngle) {
     let centerY = height / 2;
     let rayLength = 300;
     
-    // Indkommende lysstråle
     let incidentX = centerX - rayLength * cos(incidentAngle);
     let incidentY = centerY - rayLength * sin(incidentAngle);
     stroke(255, 0, 0);
     strokeWeight(3);
     line(incidentX, incidentY, centerX, centerY);
     
-    // Brydnings- eller reflekteret stråle
     if (!isNaN(refractedAngle)) {
         let refractedX = centerX + rayLength * cos(refractedAngle);
         let refractedY = centerY + rayLength * sin(refractedAngle);
         stroke(0, 0, 255);
         line(centerX, centerY, refractedX, refractedY);
     } else {
-        // Totalrefleksion langs x-aksen
         let reflectedX = centerX + rayLength * cos(incidentAngle);
         let reflectedY = centerY - rayLength * sin(incidentAngle);
         stroke(255, 165, 0);
